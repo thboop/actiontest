e378f2fa38156e83f544d38fff5069e44bcfc849
Thomas Boop <thboop@github.com>
2020-04-29 10:11:02 -0400

    test

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ async function run() {
   try {
     if (process.platform === 'win32'){
       const node12Path1 = await tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-win-x64.7z');
-      await tc.extract7z(node12Path1, 'test1');
+      //await tc.extract7z(node12Path1, 'test1');
     }
     else {
       const node12Path = await tc.downloadTool('https://nodejs.org/dist/v12.7.0/node-v12.7.0-linux-x64.tar.gz');
